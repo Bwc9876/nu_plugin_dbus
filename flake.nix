363d0d4ac4b67000565a8c18b602a42b63f7de20
Ben C <bwc9876@gmail.com>
2025-02-08 10:54:57 -0500

    Init flake, new nu version

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Nushell Plugin DBUS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flakelight.url = "github:nix-community/flakelight";
+    flakelight.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flakelight,
+  }:
+    flakelight ./. {
+      inherit inputs;
+      pname = "nu_plugin_dbus";
+      package = {
+        rustPlatform,
+        dbus,
+        nushell,
+        pkg-config,
+        fetchFromGitHub,
+        lib,
+      }:
+        rustPlatform.buildRustPackage rec {
+          pname = "nu_plugin_dbus";
+          version =
+            if nushell.version == nu_version
+            then "0.15.0"
+            else abort "Nushell Version mismatch\nPlugin: ${nu_version}\tnixpkgs: ${nushell.version}";
+          nu_version = "0.102.0";
+
+          src = ./.;
+
+          useFetchCargoVendor = true;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          nativeBuildInputs = [
+            pkg-config
+          ];
+
+          buildInputs = [
+            dbus
+          ];
+
+          meta = with lib; {
+            description = "A nushell plugin for interacting with dbus";
+            license = licenses.mit;
+            homepage = "https://github.com/devyn/nu_plugin_dbus";
+          };
+        };
+    };
+}
