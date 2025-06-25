968411194a693334b0ed0d19339cc5b269f2cad2
Ben C <bwc9876@gmail.com>
2025-06-25 07:50:50 -0400

    Update Nushell

--- flake.nix
+++ flake.nix
@@ -2,36 +2,34 @@
   description = "Nushell Plugin DBUS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flakelight.url = "github:nix-community/flakelight";
     flakelight.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    inputs @ { self
-    , nixpkgs
-    , flakelight
-    ,
-    }:
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flakelight,
+  }:
     flakelight ./. {
       inherit inputs;
       pname = "nu_plugin_dbus";
-      package =
-        { rustPlatform
-        , dbus
-        , nushell
-        , pkg-config
-        , fetchFromGitHub
-        , lib
-        ,
-        }:
+      package = {
+        rustPlatform,
+        dbus,
+        nushell,
+        pkg-config,
+        fetchFromGitHub,
+        lib,
+      }:
         rustPlatform.buildRustPackage rec {
           pname = "nu_plugin_dbus";
           version =
             if nushell.version == nu_version
-            then "0.17.0"
+            then "0.18.0"
             else abort "Nushell Version mismatch\nPlugin: ${nu_version}\tnixpkgs: ${nushell.version}";
-          nu_version = "0.104.1";
+          nu_version = "0.105.1";
 
           src = ./.;
 
