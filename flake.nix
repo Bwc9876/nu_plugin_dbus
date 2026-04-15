d0661d861e9d28cd5a0ada9c8ae9714bf52737f9
Ben C <ben@bwc9876.dev>
2026-04-15 16:45:21 -0400

    Update Nushell

--- flake.nix
+++ flake.nix
@@ -55,7 +55,7 @@
               };
             }
           );
-          nu_version = "0.111.0";
+          nu_version = "0.112.1";
         in
         if nushell.version == nu_version then
           nu_plugin_dbus
